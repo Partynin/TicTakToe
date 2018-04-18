e3e94a6099d5078ace2fc7c6fca3b367eb3436ec
Konstantin <partinin@bk.ru>
2018-04-18 19:07:17 +0400

    Add test file GameTest

---
+++ src/test/java/com/partinin/app/controller/GameTest.java
@@ -0,0 +1,12 @@
+package com.partinin.app.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void handleMouseClick() {
+    }
+}
