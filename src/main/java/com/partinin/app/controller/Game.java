03144002f6bf41f0ae8c53f852d467f9afcf3766
Konstantin <partinin@bk.ru>
2018-05-06 21:24:54 +0400

    Deleted JavadocVariable from the checkstyle.xml and add UTF-8 properties in pom.xml. Some stile change in the Game class.

--- src/main/java/com/partinin/app/controller/Game.java
+++ src/main/java/com/partinin/app/controller/Game.java
@@ -9,33 +9,35 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-/** The Game class represent controller for the game.
- *
- *  The game rules:
- *  1. Each turn, you mark one of the small squares.
- *  2. When you get three in a row on a small board, you’ve won that board.
- *  3. To win the game, you need to win three small boards in a row.
- *
- *  You don’t get to pick which of the nine boards to play on. That’s determined
- *  by your opponent’s previous move. Whichever square he picks, that’s the board
- *  you must play in next.
- *
- *  What if my opponent sends me to a board that’s already been won? In that case,
- *  congratulations – you get to go anywhere you like, on any of the other boards.
- *
- *  What if one of the small boards results in a tie? I recommend that the board
- *  counts for neither X nor O. But, if you feel like a crazy variant, you could
- *  agree before the game to count a tied board for both X and O.
- *  */
+/**
+ * The Game class represent controller for the game.
+ * <p>
+ * The game rules:
+ * 1. Each turn, you mark one of the small squares.
+ * 2. When you get three in a row on a small board, you’ve won that board.
+ * 3. To win the game, you need to win three small boards in a row.
+ * <p>
+ * You don’t get to pick which of the nine boards to play on. That’s
+ * determined by your opponent’s previous move. Whichever square he picks,
+ * that’s the board you must play in next.
+ * <p>
+ * What if my opponent sends me to a board that’s already been won? In that
+ * case, congratulations – you get to go anywhere you like, on any of the
+ * other boards.
+ * <p>
+ * What if one of the small boards results in a tie? I recommend that the board
+ * counts for neither X nor O. But, if you feel like a crazy variant, you could
+ * agree before the game to count a tied board for both X and O.
+ */
 
 public class Game extends Application {
     private static final int PANE_WIDTH = 700;
     private static final int PANE_HEIGHT = 700;
-    static private BigField bigField;
-    static private BigFieldView view;
+    private static BigField bigField;
+    private static BigFieldView view;
 
     @Override
-    public void start(Stage primaryStage) {
+    public final void start(Stage primaryStage) {
         // Create a scene and place the pane in the stage
         Scene scene = new Scene(getPane(), PANE_WIDTH, PANE_HEIGHT);
         primaryStage.setTitle("Tic-Tak-Toe"); // Set the stage title
@@ -52,7 +54,8 @@ public class Game extends Application {
         return pane;
     }
 
-    public static void handleMouseClick(FieldView.Cell cell, FieldView fieldView, Field field) {
+    public static void handleMouseClick(FieldView.Cell cell,
+                                        FieldView fieldView, Field field) {
         field.setTokenInCells(cell.getPosition(), fieldView, cell);
         bigField.checkWinner();
     }
